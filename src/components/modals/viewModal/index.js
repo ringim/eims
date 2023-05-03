aed0af1d10d35dd2e4974be05aa159cb69d3ac9b
akramnarejo <ak.narejo@gmail.com>
2023-05-03 06:50:38 +0500

    feat: added 2 more surveys and renamed first survey folder

--- src/components/modals/viewModal/index.js
+++ src/components/modals/viewModal/index.js
@@ -3,7 +3,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import ATMClientSurvey from "src/surveys/atmClientSurvey";
+import ATMClientSurvey from "src/surveys/clientExitInterview";
 const style = {
   position: "absolute",
   top: "50%",
